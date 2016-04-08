9c3cad73f3b7dc46a11a6969d1cc7a4c04d82536
erik tolentino and michael lor <michael.vang.1985@gmail.com>
2016-04-07 17:17:37 -0700

    cheat codes

--- app/components/rating-display.js
+++ app/components/rating-display.js
@@ -5,10 +5,25 @@ export default Ember.Component.extend({
   totalRating: Ember.computed('product.feedback.length', function() {
     var rating = 0;
     var product = this.get('product');
-    console.log(product.feedback.rating);
+    // console.log(product.get('feedback').length);
+    debugger;
     for(var i = 0; i < product.feedback.length; i++) {
       rating += (product.get('feedback'))[i].get('rating');
     }
     return rating;
   }),
 });
+// CHEAT CODES
+// import Ember from 'ember';
+//
+// export default Ember.Component.extend({
+//   average_stars: Ember.computed('bikestore', function() {
+//     var i = 0;
+//     var j = 0;
+//     this.bikestore.get('comments').map(function(comment){
+//       j += 1;
+//       i += parseInt(comment.get('bikelike'));
+//     });
+//      return ~~(i / j);
+//   })
+// });
